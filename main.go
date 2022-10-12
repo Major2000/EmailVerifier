37575587de8076346e288730afc1be0d0975e20c
Major2000 <majormadobe@gmail.com>
2022-10-12 14:04:49 +0300

    checked mxRecord and spfRecord

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain,hasMX,hasSPF,spfRecord,hasDMARC,dmarcReord")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error: could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+	var (
+		hasMx, hasSPF, hasDMAC bool
+		spfRecord, dmacRecord  string
+	)
+
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMx = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "vspf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
 
 }
